09368e6189f73f7da648e9d2db089eca3edcdeed
bbarker <beb82@cornell.edu>
2017-12-03 07:35:10 -0500

    updating scala minor version and using scala collections as a test

--- src/main/scala/org/terasology/ScalaCommands.scala
+++ src/main/scala/org/terasology/ScalaCommands.scala
@@ -6,6 +6,11 @@ import org.terasology.logic.console.commandSystem.annotations.Command
 @RegisterSystem
 class ScalaCommands extends BaseComponentSystem {
 
+  private val helloList: List[String] = List(
+    "Hello there,",
+    "this is using Scala collections =)"
+  )
+
   @Command(shortDescription = "Scala test command", runOnServer = false)
-  def helloScala() = "Hello there, this is Scala =)"
+  def helloScala() = helloList.mkString(" ")
 }
